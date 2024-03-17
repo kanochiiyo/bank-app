54fd406e6ddf64c25b22e3a2bbf894e388259708
dinn <133855037+kanochiiyo@users.noreply.github.com>
2024-03-17 13:34:14 +0000

    commit

--- app/database.py
+++ app/database.py
@@ -19,3 +19,51 @@ class Database:
                 data.append(item) # menambahkan ke array
 
         return data
+
+    def get_by_key(self, key:str, search_value:str):
+        single_item: dict = None
+        item_line = 0
+
+        with open(self.db) as file:
+            for line in file.readlines():
+                item_line += 1
+                item = line.strip()
+                item = json.loads(item) # ubah string jadi dictionary
+
+                if item.get(key) == search_value:
+                    single_item = item.copy()
+                    break
+        return {"line": item_line, "data": single_item}
+    
+    def get_by_id(self, id:str):
+        single_item:dict = None
+        item_line = 0
+
+        with open(self.db) as file:
+            for line in file.readlines():
+                item_line += 1
+                item = line.strip()
+                item = json.loads(item)
+
+                if item.get("id") == search_value:
+                    single_item = item.copy()
+                    break
+        return {"line": item_line, "data": single_item}
+    
+    def update(self, id:str, updated_data:dict):
+        single_item:dict = self.get_by_id(id)
+
+        if not single_item.get('data'):
+            return False
+        
+        with open(self.db, 'r') as file:
+            lines = file.readlines()
+            lines[single_item['line'] - 1] = json.dumps(updated_data) + '\n' # menambahkan new line pada akhir baris
+
+            with open(self.db, "w") as file:
+                file.writelines(lines) # data disimpan dan tulis ulang
+
+        return {"line": single_item.get('line'), "data": updated_data}
+
+# get_by_key : mengambil data  dari tiap baris disesuaikan dengan key json yang sesuai. item.get(key) sifatnya dinamis
+# get_by_id : mengambil data spesifik by id
